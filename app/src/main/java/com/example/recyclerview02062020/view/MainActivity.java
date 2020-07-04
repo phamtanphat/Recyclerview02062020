4902a5ac1720f500bba1c92c0b5ffb05a72ce1a6
phatdroid94 <phatdroid94@gmail.com>
2020-07-04 19:04:03 +0700

    add array

--- app/src/main/java/com/example/recyclerview02062020/view/MainActivity.java
+++ app/src/main/java/com/example/recyclerview02062020/view/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.recyclerview02062020.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.recyclerview02062020.R;
+import com.example.recyclerview02062020.adapter.FoodAdapter;
+import com.example.recyclerview02062020.model.Food;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRcvFood;
+    List<Food> mArrayFoods;
+    FoodAdapter mFoodAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRcvFood = findViewById(R.id.recyclerviewFood);
+        mArrayFoods = new ArrayList<>();
+
+        mArrayFoods.add(new Food(R.drawable.buncha ,"Bún chả","Bún chả hà nội ngon số một khu vực Hà Nội","45000"));
+        mArrayFoods.add(new Food(R.drawable.goicuon ,"Gỏi cuốn","Gỏi cuốn tôm thịt","30000"));
+        mArrayFoods.add(new Food(R.drawable.khaivi ,"Khai vị","Các món khai vị được dùng trong bữa tiệc","130000"));
+        mArrayFoods.add(new Food(R.drawable.tomchien ,"Tôm chiên","Tôm chiên giòn","50000"));
+        mArrayFoods.add(new Food(R.drawable.buncha ,"Tôm hùm","Tôm hùm tươi sống","150000"));
     }
 }
